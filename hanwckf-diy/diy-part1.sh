6632f5476bb2a7a2e5170b40987e7e5ceb8e1871
Yuzhii0718 <yuzhii0718@outlook.com>
2026-05-04 13:46:17 +0800

    add hy3000 and sn-r1 diy files

---
+++ hanwckf-diy/diy-part1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Add a feed source
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#git clone https://github.com/messense/aliyundrive-webdav package/messense
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/animegasan/luci-app-wolplus.git package/luci-app-wolplus
+git clone https://github.com/sbwml/luci-app-openlist2 package/openlist2
+git clone https://github.com/EasyTier/luci-app-easytier package/easytier
